c257c1f6dd9ed53043799f7623663c311f56e838
Lipraty <sis1943@outlook.com>
2023-11-03 17:18:35 +0800

    chore: command of jsx

--- src/command.tsx
+++ src/command.tsx
@@ -1,6 +1,15 @@
 import { Command, Context } from "koishi";
 import { SleepManage } from "./types";
-import { getTimeByTZ } from "./utils";
+import { getTimeByTZ, timerFormat } from "./utils";
+
+
+interface ResultMessagerProps {
+  first: boolean
+  period: 'morning' | 'evening'
+  calcTime: number
+  rank?: number
+  isDirect?: boolean
+}
 
 export function apply(ctx: Context, config: SleepManage.Config) {
   const cmd = ctx.command('sleep')
@@ -20,23 +29,51 @@ export function apply(ctx: Context, config: SleepManage.Config) {
       }
     })
 
-  withSubcommand(cmd, 'morning', config,true)
-    .action(async ({ session }) => {
+  withSubcommand(cmd, 'morning', config, true)
+    .action(async ({ session, options }) => {
       session.user[SleepManage.User.Sleeping] = false
+      const calcTime = 0
+
+      return <ResultMessager 
+      first={options.first} 
+      period="morning" 
+      calcTime={calcTime} 
+      rank={options.rank ?? 0}/>
     })
 
-  withSubcommand(cmd, 'evening', config,true)
-    .action(async ({ session }) => {
+  withSubcommand(cmd, 'evening', config, true)
+    .action(async ({ session, options }) => {
       session.user[SleepManage.User.Sleeping] = true
       session.user[SleepManage.User.EveningCount]++
+      const calcTime = 0
+
+      return <ResultMessager 
+      first={options.first} 
+      period="evening" 
+      calcTime={calcTime} 
+      rank={options.rank ?? 0}/>
     })
 }
 
 function withSubcommand(cmd: Command, sub: string, config: SleepManage.Config, hidden = false) {
   return cmd.subcommand(`.${sub}`, { hidden })
     .userFields(['id', SleepManage.User.TimeZone, SleepManage.User.EveningCount, SleepManage.User.Sleeping])
+    .option('rank', '-r <rank:number>')
+    .option('first', '-f')
     .before(async ({ session, options }) => {
       const now = getTimeByTZ(session.user[SleepManage.User.TimeZone] || config.timezone === true ? new Date().getTimezoneOffset() / -60 : config.timezone)
-      
+
     })
+}
+
+function ResultMessager({ first, period, calcTime, rank, isDirect }: ResultMessagerProps) {
+  return (<message>{
+    first
+      ? <text period={period} path={'first'}></text>
+      : <>
+        <text period={period} path={'reply'}></text>
+        <text period={period} path={'timer'} args={timerFormat(calcTime, true)}></text>
+        {isDirect ?? <text period={period} path={'rank'} args={[rank]}></text>}
+      </>
+  }</message>)
 }
